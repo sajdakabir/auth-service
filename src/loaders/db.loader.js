d61131e81b898152a9f502920384d9d26a8de8e4
Sajda Kabir <sajdakabir@Sajdas-MacBook-Air.local>
2023-09-22 15:55:38 +0530

    connected to mongodb

---
+++ src/loaders/db.loader.js
@@ -0,0 +1,6 @@
+import { environment } from "./environment.loader.js"
+const db = createConnection(`mongodb+srv://${environment.DB_USER}:${environment.DB_PASS}@${environment.DB_HOST}/${environment.DB_NAME}`, {
+    autoIndex: false
+})
+
+export { db }

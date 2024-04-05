21d639c9fcbebe6ede942b39523e8f050a2fa230
sajdkabir <sajda.kbir@gmail.com>
2024-04-05 23:49:13 +0530

    fix: magic link bug

--- src/routes/core/auth.route.js
+++ src/routes/core/auth.route.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { registerEmailUserController, emailLoginController } from "../../controllers/auth.controller.js";
+import { registerEmailUserController, emailLoginController, magicLoginController } from "../../controllers/auth.controller.js";
 const router = Router();
 
 router.route('/common/signup').post(registerEmailUserController);
 router.route('/common/login/').post(emailLoginController);
+router.route('/magic/login/').post(magicLoginController);
 
 export default router;
